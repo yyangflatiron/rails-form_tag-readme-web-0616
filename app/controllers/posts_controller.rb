e1e8c0b67522754a1699caedcf85858a0b30188a
yyangflatiron <yichen.yang@flatironschool.com>
2016-07-05 13:30:47 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,13 @@
 class PostsController < ApplicationController
 
+	def create
+		@post = params
+  		redirect_to new_post_path(post: @post)
+		# @post = Post.create(params)
+		# redirect to 'posts/#{@post.id}'
+	end
+
+	def new
+	end
+
 end
